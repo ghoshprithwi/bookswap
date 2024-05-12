adee5fad7f0667e589bbc8a4be468a0d992a2e58
abhaykasavaraju <abhaykasavaraju@gmail.com>
2024-05-12 13:23:34 +0530

    Integration Initial

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -4,39 +4,37 @@ const mongoose = require('mongoose');
 const UsersSchema = new mongoose.Schema({
     userid: {
      type: String,
-      required: true 
     },
     password: {
       type: String,
-       required: true 
      },    
-    name: {
+    firstName: {
       type: String,
-      required: true
+    },
+    lastName: {
+      type: String,
     },
     email: {
       type: String,
-      required: true
     },
     ownedbooks: {
-        type: [String],
-        required: true
+        type: [String]
     },
     borrowedbooks: {
       type: [String],
-      required: true
     },
     requestedbooks: {
       type: [String],
-      required: true
     },
     location: {
       type: String,
-      required: true,
     },
     genres: {
       type: [String],
     },
+    phone: {
+      type: String,
+    }
   });
   
   module.exports = User = mongoose.model('User', UsersSchema);
